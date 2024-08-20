f71d5d4f7541d31b992e7c8555b101483824e2e7
fancn21th <fancn21th@gmail.com>
2024-08-20 15:00:46 +0800

    📦 NEW: 基本布局

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="text-red-600">Hello Tailwind</div>;
+  return (
+    <div className="w-screen h-screen">
+      <div className="w-full h-full bg-slate-100">消息历史</div>
+      <div className="fixed bottom-0 w-full flex justify-center bg-blue-100">
+        <textarea className="">请输入消息...</textarea>
+      </div>
+    </div>
+  );
 }
 
 export default App;
